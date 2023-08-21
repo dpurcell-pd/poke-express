d242ec81352edbe2be0c2fa8a3b954235412b15a
dpurcell <dpurcell.pd@gmail.com>
2023-08-21 07:03:39 -0400

    my server is set up and running

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+
+app.get('/', (req, res) => {
+    res.send("<h1>Welcome to the Pokemon App!</h1>")
+});
+
+app.listen(port, (req, res) => {
+    console.log(`Listening on port ${port}.`);   
+});
